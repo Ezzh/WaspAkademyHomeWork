62a13371aefad9f69bac45bbc1a95d4073721c3f
nikitababkov <ezzh32@yandex.ru>
2022-11-26 00:38:35 +0300

    final
    final

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+namespace HomeworkOOP
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Truck gb = new Truck("Lamba", 123, 1456, 65, "Goblin");
+            PassagerCar hob = new PassagerCar("Javelin", 100, 1567, 45);
+            PassagerCar ferra = new PassagerCar("Ferra", 100, 1890, 3);
+            Autopark park = new Autopark("Dota");
+            park.AddCar(gb);
+            park.AddCar(hob);
+            park.AddCar(ferra);
+            Console.WriteLine(park);
+
+        }
+    }
+}
