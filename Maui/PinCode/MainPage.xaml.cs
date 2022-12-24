8b6c99a4e5c9623a7694908b874e3fe700ccb538
Ezzh <74453538+Ezzh@users.noreply.github.com>
2022-12-25 01:39:34 +0300

    Maui pincode

---
+++ Maui/PinCode/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using static Android.Renderscripts.ScriptGroup;
+
+namespace Maui;
+
+public partial class MainPage : ContentPage
+{
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+        if (result.IsEnabled) result.Text += ((Button)sender).Text;
+    }
+
+    private void Delete_Clicked(object sender, EventArgs e)
+    {
+        if (result.IsEnabled) result.Text = "";
+    }
+
+    private void Access_Clicked(object sender, EventArgs e)
+    {
+        if (result.IsEnabled)
+        {
+            if (result.Text == "1234")
+            {
+                result.Text = "ACCESS!";
+                result.IsEnabled = false;
+            }
+            else
+            {
+                result.Text = "";
+            }
+        }
+    }
+}
+
